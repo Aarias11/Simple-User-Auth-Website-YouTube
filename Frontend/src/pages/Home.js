e99e1a8511fd573807425a4d68767a6f483a0ae8
Aarias11 <108088290+Aarias11@users.noreply.github.com>
2023-08-13 15:33:46 -0400

    Add files via upload

---
+++ Frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-full h-screen bg-[#1a1a1a] text-white flex justify-center items-center'>
+      <h2 className='text-3xl'>HOME</h2>
+    </div>
+  )
+}
+
+export default Home
